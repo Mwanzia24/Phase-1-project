f45009e91b14b9818e7f317946ce4a471f486874
tom mwanzia <mwanziatom19@gmail.com>
2023-04-09 23:27:37 +0300

    updated

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ function fetchBookDetailsById(id) {
   fetch(url)
     .then(response => response.json())
     .then(book => {
-      console.log(book); // replace with your code to display book details
+      console.log(book); 
     })
     .catch(error => console.error(error));
 }
 
-// Example usage
+
 const searchButton = document.getElementById("searchButton");
 const bookIdInput = document.getElementById("bookIdInput");
 
