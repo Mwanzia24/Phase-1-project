474bfc846f0b0837b38ed290d0307aa41c1f977e
tom mwanzia <mwanziatom19@gmail.com>
2023-04-12 13:40:09 +0300

    done

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
-const bookDetailsUrl = "http://localhost:3000/books/";
+const bookListUrl = "http://localhost:3000/books/";
 
-function fetchBookDetailsById(id) {
-  const url = bookDetailsUrl + id;
+function fetchBooks(query) {
+  const url = bookListUrl + "?q=" + query;
   
   fetch(url)
     .then(response => response.json())
-    .then(book => {
-      console.log(book); 
+    .then(data => {
+      const books = data.books;
+      populateTable(books);
     })
     .catch(error => console.error(error));
 }
 
+function populateTable(books) {
+  const bookTableBody = document.getElementById("bookTableBody");
+  bookTableBody.innerHTML = "";
+  
+  for (let book of books) {
+    const row = document.createElement("tr");
+    
+    const titleCell = document.createElement("td");
+    titleCell.innerText = book.title;
+    row.appendChild(titleCell);
+    
+    const authorCell = document.createElement("td");
+    authorCell.innerText = book.author;
+    row.appendChild(authorCell);
+    
+    const isbnCell = document.createElement("td");
+    isbnCell.innerText = book.isbn;
+    row.appendChild(isbnCell);
+    
+    const actionsCell = document.createElement("td");
+    const borrowButton = document.createElement("button");
+    borrowButton.innerText = "Borrow";
+    actionsCell.appendChild(borrowButton);
+    row.appendChild(actionsCell);
+    
+    bookTableBody.appendChild(row);
+  }
+}
 
 const searchButton = document.getElementById("searchButton");
-const bookIdInput = document.getElementById("bookIdInput");
+const searchInput = document.getElementById("searchInput");
 
-searchButton.addEventListener("click", () => {
-  const bookId = bookIdInput.value;
-  fetchBookDetailsById(bookId);
+searchButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const query = searchInput.value;
+  fetchBooks(query);
 });
